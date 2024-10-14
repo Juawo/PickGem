40d5cf40441c38e61dc249ee4818a5a6efd91a92
João Pedro R. Medeiros de Amorim <joaopedrojp2703@gmail.com>
2024-10-14 18:58:20 -0300

    Adicionando Levels 1 a 3 e modelo do 4 e 5

---
+++ pickgemgame/Scripts/death_area.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@onready var reset_timer: Timer = $reset_timer
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Players"):
+		reset_timer.start(2.0)
+
+func _on_reset_timer_timeout() -> void:
+	get_tree().reload_current_scene()
