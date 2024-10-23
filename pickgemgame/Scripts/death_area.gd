84e404e9d5f17733ae0e523be909a4c8e1128a25
João Pedro R. Medeiros de Amorim <joaopedrojp2703@gmail.com>
2024-10-23 18:08:37 -0300

    Alterando forma de morrer do player1 e alterando forma de aparecer particles

--- pickgemgame/Scripts/death_area.gd
+++ pickgemgame/Scripts/death_area.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-@onready var reset_timer: Timer = $reset_timer
 @onready var cpu_particles_p1: CPUParticles2D = $cpu_particles_P1
 @onready var cpu_particles_p2: CPUParticles2D = $cpu_particles_P2
 
@@ -8,18 +7,13 @@ var player_is_death : CharacterBody2D
 
 func _on_body_entered(body: CharacterBody2D) -> void:
 	if body.is_in_group("Players"):
-		death_particle(body)
-		reset_timer.start(3.0)
+		body.player_death()
 
-func death_particle(player: CharacterBody2D):
-	if player.is_in_group("Players"):
-		if player.name == "player_1":
-			cpu_particles_p1.global_position.x = player.global_position.x
-			cpu_particles_p1.emitting = true
-		if player.name == "player_2":
-			cpu_particles_p2.global_position.x = player.global_position.x
-			cpu_particles_p2.emitting = true
-
-
-func _on_reset_timer_timeout() -> void:
-	get_tree().reload_current_scene()
+#func death_particle(player: CharacterBody2D):
+	#if player.is_in_group("Players"):
+		#if player.name == "player_1":
+			#cpu_particles_p1.global_position.x = player.global_position.x
+			#cpu_particles_p1.emitting = true
+		#if player.name == "player_2":
+			#cpu_particles_p2.global_position.x = player.global_position.x
+			#cpu_particles_p2.emitting = true
--- pickgemgame/Scripts/player_1.gd
+++ pickgemgame/Scripts/player_1.gd
@@ -1,24 +1,57 @@
 extends CharacterBody2D
 
+@onready var respawn_timer: Timer = $respawn_timer
+@onready var collision: CollisionShape2D = $collision
+@onready var cpu_particles_p_1: CPUParticles2D = $cpu_particles_P1
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+@export var respawn_time : float = 1.5
 
 const SPEED = 220.0
 const JUMP_VELOCITY = -280.0
+
 var score : int = 0
+var initial_position : Vector2
+var is_death := false
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func _ready() -> void:
+	initial_position = self.global_position
 	
-	# Handle jump.
-	if Input.is_action_just_pressed("move_up_player1") and is_on_floor():
-		if Engine.time_scale != 0:
-			velocity.y = JUMP_VELOCITY
+func _physics_process(delta: float) -> void:
+	if !is_death:
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+		
+		if Input.is_action_just_pressed("move_up_player1") and is_on_floor():
+			if Engine.time_scale != 0:
+				velocity.y = JUMP_VELOCITY
 
-	var direction := Input.get_axis("move_left_player1", "move_right_player1")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		var direction := Input.get_axis("move_left_player1", "move_right_player1")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+		move_and_slide()
+
+func player_death():
+	is_death = true
+	sprite_2d.hide()
+	collision.disabled = is_death
+	respawn_timer.start(respawn_time)
+	player_particles()
+
+func player_respawn():
+	is_death = false
+	collision.disabled = is_death
+	velocity = Vector2.ZERO
+	global_position = initial_position
+	sprite_2d.show()
+
+func player_particles():
+	print('emitiu')
+	cpu_particles_p_1.emitting = true
+	cpu_particles_p_1.global_position.x = self.global_position.x
+	
+func _on_respawn_timer_timeout() -> void:
+	player_respawn()
