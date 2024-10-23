9e31f273b7055ae46edceff5a1b26efa3cb3d600
João Pedro R. Medeiros de Amorim <joaopedrojp2703@gmail.com>
2024-10-23 18:27:52 -0300

    Iniciando lógica de herança dos Players

---
+++ pickgemgame/Scenes/Players/player_2_child.gd
@@ -0,0 +1,18 @@
+extends "res://Scripts/player_bases.gd"
+
+func _process(delta: float) -> void:
+	if !is_death:
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+		
+		if Input.is_action_just_pressed("move_up_player2") and is_on_floor():
+			if Engine.time_scale != 0:
+				velocity.y = JUMP_VELOCITY
+
+		var direction := Input.get_axis("move_left_player2", "move_right_player2")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+
+		move_and_slide()
---
+++ pickgemgame/Scripts/player_1_child.gd
@@ -0,0 +1,18 @@
+extends "res://Scripts/player_bases.gd"
+
+func _process(delta: float) -> void:
+	if !is_death:
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+		
+		if Input.is_action_just_pressed("move_up_player1") and is_on_floor():
+			if Engine.time_scale != 0:
+				velocity.y = JUMP_VELOCITY
+
+		var direction := Input.get_axis("move_left_player1", "move_right_player1")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+
+		move_and_slide()
